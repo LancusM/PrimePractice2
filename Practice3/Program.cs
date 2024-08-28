6c8379c10b08c63d4a6026b41d46f9a765c4824e
Lancus Maxwell <youngsorcerer23@gmail.com>
2024-08-28 14:23:26 -0500

    Add project files.

---
+++ Practice3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int givenNum = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= givenNum; i++)
+            {
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+
+                    sum = sum + i;
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
